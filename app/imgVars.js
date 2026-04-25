48d6ead47a03c6f810bc6d1035f0b72b0c2b5d4f
Valentyna <valentine177@gmail.com>
2026-04-25 15:57:16 +0300

    Cloudinary

---
+++ app/imgVars.js
@@ -0,0 +1,8 @@
+export const IMG_VARS = {
+    STORK1X: "https://res.cloudinary.com/dxfmtmmae/image/upload/v1777120612/leleka1x_qwowqi.avif",
+    STORK2X: "https://res.cloudinary.com/dxfmtmmae/image/upload/v1777120612/leleka2x_ckwf0j.avif",
+    PLANT1X: "https://res.cloudinary.com/dxfmtmmae/image/upload/v1777120612/plant1x_of08zu.avif",
+    PLANT2X: "https://res.cloudinary.com/dxfmtmmae/image/upload/v1777120612/plant2x_ospygw.avif",
+    EGGS1X: "https://res.cloudinary.com/dxfmtmmae/image/upload/v1777120612/eggs1x_uwko5e.avif",
+    EGGS2X: "https://res.cloudinary.com/dxfmtmmae/image/upload/v1777120612/eggs2x_dpqnbd.avif"
+}
