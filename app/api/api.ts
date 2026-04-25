3c4678567b6377cc0ae3b5accd83c5f7561389eb
TeaRediT <vlad.bobro.02@gmail.com>
2026-04-25 13:13:21 +0300

    AuthProvider small fix

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "project-1-back.onrender.com/api",
   withCredentials: true,
 });
---
+++ components/layout/AuthProvider/AuthProvider.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { checkSession, getUser } from "@/lib/api/clientApi";
+import { useAuthStore } from "@/lib/store/authStore";
+import { useEffect, useState } from "react";
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  const setUser = useAuthStore((state) => state.setUser);
+  const clearIsAuthenticated = useAuthStore(
+    (state) => state.clearIsAuthenticated,
+  );
+
+  const [isChecking, setIsCheacking] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setError(false);
+      setIsCheacking(true);
+
+      try {
+        const isAuthenticated = await checkSession();
+        console.log("cheak done", isAuthenticated);
+
+        if (isAuthenticated) {
+          const user = await getUser();
+          console.log(user);
+
+          if (user) setUser(user);
+          else clearIsAuthenticated();
+        }
+      } catch {
+        setError(true);
+      } finally {
+        setIsCheacking(false);
+      }
+    };
+    fetchUser();
+  }, [clearIsAuthenticated, setUser]);
+
+  return (
+    <>
+      {error && <p>Error!!!</p>}
+      {isChecking && <p>Loading...</p>}
+      {!error && !isChecking && children}
+    </>
+  );
+};
+
+export default AuthProvider;
