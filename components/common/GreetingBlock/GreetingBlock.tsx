255b4ac0f3ce77e98b083b037573c68e18e700d1
Uffaser <gerasimenkom405@gmail.com>
2026-04-27 00:49:48 +0300

    refactor: simplify getUser function by removing unnecessary headers

--- components/common/GreetingBlock/GreetingBlock.tsx
+++ components/common/GreetingBlock/GreetingBlock.tsx
@@ -3,6 +3,7 @@ import css from "./GreetingBlock.module.css";
 
 export default async function GreetingBlock() {
   const { name } = await getUser();
+  console.log(name);
 
   return <h1 className={css.greetingTitle}>Вітаю, {name}!</h1>;
 }
--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -8,12 +8,7 @@ export interface UserCreds {
 }
 
 export const getUser = async (): Promise<User> => {
-  const { data } = await nextServer.get<User>("/users/current", {
-    headers: {
-      Authorization: 'UF00fu1z1zC4vNxL5Wi8RrI1IvvT%2B8AMcoLxf%2B7N; Path=/; Secure; HttpOnly; Expires=Sun, 26 Apr 2026 21:02:08 GMT'
-    }
-  }
-  );
+  const { data } = await nextServer.get<User>("/users/current");
   return data;
 };
 
