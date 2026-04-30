df9cec29e217eb4e1ca97fc14a8697e8fe45db9d
Viktor <143907417+Viktor-Yashchuk@users.noreply.github.com>
2026-05-01 01:22:54 +0200

    Refactor CalendarDatePicker and OnboardingClient components; update validation schema and add date picker styling

--- components/profile/OnboardingForm/OnboardingClient.tsx
+++ components/profile/OnboardingForm/OnboardingClient.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Formik, Form, FormikHelpers, ErrorMessage } from 'formik';
+import { Formik, Form, FormikHelpers, ErrorMessage, Field } from 'formik';
 import AvatarPicker from '@/components/common/AvatarPicker/AvatarPicker';
 import CalendarDatePicker from '@/components/common/CalendarDatePicker/CalendarDatePicker';
 import { FORTY_WEEKS, validationSchema } from './OnboardingValidation';
@@ -12,71 +12,82 @@ import { nextServer } from '@/lib/api/api';
 import { useAuthStore } from '@/lib/store/authStore';
 import { getUser } from '@/lib/api/clientApi';
 import Button from '@/components/common/Button/Button';
+import { IMask, IMaskInput } from 'react-imask';
 
 interface OnboardingFormValues {
-  gender: string;
-  dueDate: string;
+    gender: string;
+    dueDate: string;
 }
 
 const initialValues: OnboardingFormValues = {
-  gender: '',
-  dueDate: '',
+    gender: '',
+    dueDate: '',
 };
 
 export default function OnboardingClient() {
-  const setUser = useAuthStore((state) => state.setUser);
-  const user = useAuthStore((state) => state.user);
-  const router = useRouter();
-  const today = new Date();
-  const maxDate = new Date(Date.now() + FORTY_WEEKS);
-  const handleSubmit = async (
-    values: OnboardingFormValues,
-    actions: FormikHelpers<OnboardingFormValues>
-  ) => {
-    try {
-      // if (avatarFile) {
-      //     const formData = new FormData();
-      //     formData.append('avatar', avatarFile);
-      // await nextServer.patch('/users/me/avatar', formData);
-      //     const { data } = await nextServer.patch('/users/me', {
-      //         gender: values.gender || null,
-      //         date: values.dueDate,
-      //     });
-      //     setUser(data);
-      // }
-      await nextServer.patch('/users/me', {
-        gender: values.gender === 'unknown' ? null : values.gender,
-        date: values.dueDate,
-      });
-      const updateUser = await getUser();
-      setUser(updateUser);
-      actions.resetForm();
-      router.push('/');
-    } catch {
-      toast.error('Щось пішло не так. Спробуйте ще раз.');
-    }
-  };
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      <Form className={css.form}>
-        <AvatarPicker profilePhotoUrl={user?.avatar} />
-        <GenderSelect />
-        <ErrorMessage name="gender" component="p" />
-        <CalendarDatePicker minDate={today} maxDate={maxDate} />
-        <ErrorMessage name="dueDate" component="p" />
-        <Button
-          className={css.submitBtn}
-          type="submit"
-          variant="normal"
-          size="lg"
+    const setUser = useAuthStore((state) => state.setUser);
+    const user = useAuthStore((state) => state.user);
+    const router = useRouter();
+    const today = new Date();
+    const maxDate = new Date(Date.now() + FORTY_WEEKS);
+    const handleSubmit = async (
+        values: OnboardingFormValues,
+        actions: FormikHelpers<OnboardingFormValues>
+    ) => {
+        try {
+            await nextServer.patch('/users/me', {
+                gender: values.gender === 'unknown' || values.gender === '' ? null : values.gender,
+                date: values.dueDate || new Date(Date.now()+FORTY_WEEKS).toISOString().split('T')[0],
+            });
+            const updateUser = await getUser();
+            setUser(updateUser);
+            actions.resetForm();
+            router.push('/');
+        } catch {
+            toast.error('Щось пішло не так. Спробуйте ще раз.');
+        }
+    };
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
         >
-          Зберегти
-        </Button>
-      </Form>
-    </Formik>
-  );
+            <Form className={css.form}>
+                <AvatarPicker profilePhotoUrl={user?.avatar} />
+                <GenderSelect />
+                <ErrorMessage name="gender" component="p" />
+                <Field
+                    name='dueDate'
+                    component={CalendarDatePicker}
+                    placeholderText='Оберіть дату'
+                    className={css.datePicker}
+                    minDate={today}
+                    maxDate={maxDate}
+                    dateFormat="dd-MM-yyyy"
+                    label="Планова дата пологів"
+                    labelClassName={css.label}
+                    customInput={
+                        <IMaskInput
+                            mask="DD-MM-YYYY"
+                            blocks={{
+                                DD: { mask: IMask.MaskedRange, from: 1, to: 31 },
+                                MM: { mask: IMask.MaskedRange, from: 1, to: 12 },
+                                YYYY: { mask: IMask.MaskedRange, from: new Date().getFullYear(), to: new Date().getFullYear() + 1 },
+                            }}
+                        />
+                    }
+                />
+                <ErrorMessage name="dueDate" component="p" />
+                <Button
+                    className={css.submitBtn}
+                    type="submit"
+                    variant="normal"
+                    size="lg"
+                >
+                    Зберегти
+                </Button>
+            </Form>
+        </Formik>
+    )
 }
