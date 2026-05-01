f55703775ce0efa58543a5167249ef26429fd611
TeaRediT <vlad.bobro.02@gmail.com>
2026-05-01 14:19:12 +0300

    add loader logic

--- components/auth/LoginForm/LoginForm.tsx
+++ components/auth/LoginForm/LoginForm.tsx
@@ -1,26 +1,28 @@
-import { loginUser, UserLogCreds } from "@/lib/api/clientApi";
-import css from "./LoginForm.module.css";
-import { Formik, Form, Field, FieldProps } from "formik";
-import { useId } from "react";
-import * as Yup from "yup";
-import { useMutation } from "@tanstack/react-query";
-import { useAuthStore } from "@/lib/store/authStore";
-import Button from "@/components/common/Button/Button";
-import { useRouter } from "next/navigation";
+import { loginUser, UserLogCreds } from '@/lib/api/clientApi';
+import css from './LoginForm.module.css';
+import { Formik, Form, Field, FieldProps } from 'formik';
+import { useId } from 'react';
+import * as Yup from 'yup';
+import { useMutation } from '@tanstack/react-query';
+import { useAuthStore } from '@/lib/store/authStore';
+import Button from '@/components/common/Button/Button';
+import { useRouter } from 'next/navigation';
+import Loader from '@/components/common/Loader/Loader';
+import { createPortal } from 'react-dom';
 
 const initialValues: UserLogCreds = {
-  email: "",
-  password: "",
+  email: '',
+  password: '',
 };
 
 const formSchema = Yup.object().shape({
   email: Yup.string()
-    .email("Некоректний формат електронної пошти")
-    .max(64, "Пошта не може перевищувати 64 символи")
+    .email('Некоректний формат електронної пошти')
+    .max(64, 'Пошта не може перевищувати 64 символи')
     .required("Обов'язково поле"),
   password: Yup.string()
-    .min(8, "Пароль має містити мінімум 8 символів")
-    .max(128, "Пароль не може перевищувати 128 символів")
+    .min(8, 'Пароль має містити мінімум 8 символів')
+    .max(128, 'Пароль не може перевищувати 128 символів')
     .required("Обов'язково поле"),
 });
 
@@ -29,71 +31,82 @@ const LoginForm = () => {
   const router = useRouter();
   const setUser = useAuthStore((state) => state.setUser);
 
-  const loginMutation = useMutation({
+  const { mutate, isPending } = useMutation({
     mutationFn: loginUser,
     onSuccess: (data) => {
       setUser(data);
-      router.push("/");
+      router.push('/');
     },
     onError: () => {
-      console.log("має бути пуш повідомлення з помилкою");
+      console.log('має бути пуш повідомлення з помилкою');
     },
   });
 
   const handleSubmit = (values: UserLogCreds): void => {
-    loginMutation.mutate(values);
+    mutate(values);
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
-      validationSchema={formSchema}
-    >
-      <Form className={css.form}>
-        <div className={css["field-set"]}>
-          <label htmlFor={`${fieldId}-email`}></label>
-          <Field name="email">
-            {({ field, meta }: FieldProps) => {
-              const hasError = meta.touched && meta.error;
-              return (
-                <>
-                  <input
-                    {...field}
-                    type="email"
-                    id={`${fieldId}-email`}
-                    placeholder={"Пошта"}
-                    className={`${css.input} ${hasError ? css["input-error"] : ""}`}
-                  />
-                  {hasError && <span className={css.span}>{meta.error}</span>}
-                </>
-              );
-            }}
-          </Field>
-        </div>
-        <div className={css["field-set"]}>
-          <label htmlFor={`${fieldId}-password`}></label>
-          <Field name="password">
-            {({ field, meta }: FieldProps) => {
-              const hasError = meta.touched && meta.error;
-              return (
-                <>
-                  <input
-                    {...field}
-                    type="password"
-                    id={`${fieldId}-password`}
-                    placeholder={"Пароль"}
-                    className={`${css.input} ${hasError ? css["input-error"] : ""}`}
-                  />
-                  {hasError && <span className={css.span}>{meta.error}</span>}
-                </>
-              );
-            }}
-          </Field>
-        </div>
-        <Button type="submit">Увійти</Button>
-      </Form>
-    </Formik>
+    <>
+      {isPending &&
+        createPortal(
+          <Loader theme={'default'} variant="global" />,
+          document.body
+        )}
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={formSchema}
+      >
+        <Form className={css.form}>
+          <div className={css['field-set']}>
+            <label htmlFor={`${fieldId}-email`}></label>
+            <Field name="email">
+              {({ field, meta }: FieldProps) => {
+                const hasError = meta.touched && meta.error;
+                return (
+                  <>
+                    <input
+                      {...field}
+                      type="email"
+                      id={`${fieldId}-email`}
+                      placeholder={'Пошта'}
+                      autoComplete={'email'}
+                      className={`${css.input} ${hasError ? css['input-error'] : ''}`}
+                    />
+                    {hasError && <span className={css.span}>{meta.error}</span>}
+                  </>
+                );
+              }}
+            </Field>
+          </div>
+          <div className={css['field-set']}>
+            <label htmlFor={`${fieldId}-password`}></label>
+            <Field name="password">
+              {({ field, meta }: FieldProps) => {
+                const hasError = meta.touched && meta.error;
+                return (
+                  <>
+                    <input
+                      {...field}
+                      type="password"
+                      id={`${fieldId}-password`}
+                      placeholder={'Пароль'}
+                      autoComplete={'current-password'}
+                      className={`${css.input} ${hasError ? css['input-error'] : ''}`}
+                    />
+                    {hasError && <span className={css.span}>{meta.error}</span>}
+                  </>
+                );
+              }}
+            </Field>
+          </div>
+          <Button type="submit" disabled={isPending}>
+            Увійти
+          </Button>
+        </Form>
+      </Formik>
+    </>
   );
 };
 
--- components/auth/RegistrationForm/RegistrationForm.tsx
+++ components/auth/RegistrationForm/RegistrationForm.tsx
@@ -1,30 +1,32 @@
-import { registerUser, UserRegCreds } from "@/lib/api/clientApi";
-import css from "./RegistrationForm.module.css";
-import { Formik, Form, Field, FieldProps } from "formik";
-import { useId } from "react";
-import * as Yup from "yup";
-import { useMutation } from "@tanstack/react-query";
-import { useAuthStore } from "@/lib/store/authStore";
-import Button from "@/components/common/Button/Button";
-import { useRouter } from "next/navigation";
+import { registerUser, UserRegCreds } from '@/lib/api/clientApi';
+import css from './RegistrationForm.module.css';
+import { Formik, Form, Field, FieldProps } from 'formik';
+import { useId } from 'react';
+import * as Yup from 'yup';
+import { useMutation } from '@tanstack/react-query';
+import { useAuthStore } from '@/lib/store/authStore';
+import Button from '@/components/common/Button/Button';
+import { useRouter } from 'next/navigation';
+import { createPortal } from 'react-dom';
+import Loader from '@/components/common/Loader/Loader';
 
 const initialValues: UserRegCreds = {
-  name: "",
-  email: "",
-  password: "",
+  name: '',
+  email: '',
+  password: '',
 };
 
 const formSchema = Yup.object().shape({
   name: Yup.string()
-    .max(32, "Імʼя не може перевищувати 32 символи")
+    .max(32, 'Імʼя не може перевищувати 32 символи')
     .required("Обов'язково поле"),
   email: Yup.string()
-    .email("Некоректний формат електронної пошти")
-    .max(64, "Пошта не може перевищувати 64 символи")
+    .email('Некоректний формат електронної пошти')
+    .max(64, 'Пошта не може перевищувати 64 символи')
     .required("Обов'язково поле"),
   password: Yup.string()
-    .min(8, "Пароль має містити мінімум 8 символів")
-    .max(128, "Пароль не може перевищувати 128 символів")
+    .min(8, 'Пароль має містити мінімум 8 символів')
+    .max(128, 'Пароль не може перевищувати 128 символів')
     .required("Обов'язково поле"),
 });
 
@@ -33,91 +35,103 @@ const RegistrationForm = () => {
   const router = useRouter();
   const setUser = useAuthStore((state) => state.setUser);
 
-  const registerMutation = useMutation({
+  const { mutate, isPending } = useMutation({
     mutationFn: registerUser,
     onSuccess: (data) => {
       setUser(data);
-      router.push("/profile/edit");
+      router.push('/profile/edit');
     },
     onError: () => {
-      console.log("має бути пуш повідомлення з помилкою");
+      console.log('має бути пуш повідомлення з помилкою');
     },
   });
 
   const handleSubmit = (values: UserRegCreds): void => {
-    registerMutation.mutate(values);
+    mutate(values);
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
-      validationSchema={formSchema}
-    >
-      <Form className={css.form}>
-        <div className={css["field-set"]}>
-          <label htmlFor={`${fieldId}-title`}>Імʼя*</label>
-          <Field name="name">
-            {({ field, meta }: FieldProps) => {
-              const hasError = meta.touched && meta.error;
-              return (
-                <>
-                  <input
-                    {...field}
-                    type="text"
-                    id={`${fieldId}-title`}
-                    placeholder={"Ваше імʼя"}
-                    className={`${css.input} ${hasError ? css["input-error"] : ""}`}
-                  />
-                  {hasError && <span className={css.span}>{meta.error}</span>}
-                </>
-              );
-            }}
-          </Field>
-        </div>
-        <div className={css["field-set"]}>
-          <label htmlFor={`${fieldId}-email`}>Пошта*</label>
-          <Field name="email">
-            {({ field, meta }: FieldProps) => {
-              const hasError = meta.touched && meta.error;
-              return (
-                <>
-                  <input
-                    {...field}
-                    type="email"
-                    id={`${fieldId}-email`}
-                    placeholder={"hello@leleka.com"}
-                    className={`${css.input} ${hasError ? css["input-error"] : ""}`}
-                  />
-                  {hasError && <span className={css.span}>{meta.error}</span>}
-                </>
-              );
-            }}
-          </Field>
-        </div>
-        <div className={css["field-set"]}>
-          <label htmlFor={`${fieldId}-password`}>Пароль*</label>
-          <Field name="password">
-            {({ field, meta }: FieldProps) => {
-              const hasError = meta.touched && meta.error;
-              return (
-                <>
-                  <input
-                    {...field}
-                    type="password"
-                    id={`${fieldId}-password`}
-                    placeholder={"********"}
-                    className={`${css.input} ${hasError ? css["input-error"] : ""}`}
-                  />
-                  {hasError && <span className={css.span}>{meta.error}</span>}
-                </>
-              );
-            }}
-          </Field>
-        </div>
-        <Button type="submit">Зареєструватись</Button>
-      </Form>
-    </Formik>
+    <>
+      {isPending &&
+        createPortal(
+          <Loader theme={'default'} variant="global" />,
+          document.body
+        )}
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={formSchema}
+      >
+        <Form className={css.form}>
+          <div className={css['field-set']}>
+            <label htmlFor={`${fieldId}-title`}>Імʼя*</label>
+            <Field name="name">
+              {({ field, meta }: FieldProps) => {
+                const hasError = meta.touched && meta.error;
+                return (
+                  <>
+                    <input
+                      {...field}
+                      type="text"
+                      id={`${fieldId}-title`}
+                      placeholder={'Ваше імʼя'}
+                      autoComplete={'username'}
+                      className={`${css.input} ${hasError ? css['input-error'] : ''}`}
+                    />
+                    {hasError && <span className={css.span}>{meta.error}</span>}
+                  </>
+                );
+              }}
+            </Field>
+          </div>
+          <div className={css['field-set']}>
+            <label htmlFor={`${fieldId}-email`}>Пошта*</label>
+            <Field name="email">
+              {({ field, meta }: FieldProps) => {
+                const hasError = meta.touched && meta.error;
+                return (
+                  <>
+                    <input
+                      {...field}
+                      type="email"
+                      id={`${fieldId}-email`}
+                      placeholder={'hello@leleka.com'}
+                      autoComplete={'email'}
+                      className={`${css.input} ${hasError ? css['input-error'] : ''}`}
+                    />
+                    {hasError && <span className={css.span}>{meta.error}</span>}
+                  </>
+                );
+              }}
+            </Field>
+          </div>
+          <div className={css['field-set']}>
+            <label htmlFor={`${fieldId}-password`}>Пароль*</label>
+            <Field name="password">
+              {({ field, meta }: FieldProps) => {
+                const hasError = meta.touched && meta.error;
+                return (
+                  <>
+                    <input
+                      {...field}
+                      type="password"
+                      id={`${fieldId}-password`}
+                      placeholder={'********'}
+                      autoComplete={'new-password'}
+                      className={`${css.input} ${hasError ? css['input-error'] : ''}`}
+                    />
+                    {hasError && <span className={css.span}>{meta.error}</span>}
+                  </>
+                );
+              }}
+            </Field>
+          </div>
+          <Button type="submit" disabled={isPending}>
+            Зареєструватись
+          </Button>
+        </Form>
+      </Formik>
+    </>
   );
 };
 
