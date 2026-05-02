bbb833ddb5207daabe4b86a60c2abb39045735a4
Uffaser <gerasimenkom405@gmail.com>
2026-05-02 20:44:51 +0300

    Add MultiSelect component and integrate with BabyTodayCard; implement emotions fetching and styling

---
+++ components/common/CustomCheckbox/CustomCheckbox.tsx
@@ -0,0 +1,36 @@
+import css from './CustomCheckbox.module.css';
+
+type Props = {
+  id: string;
+  checked?: boolean;
+  handleCheck?: () => void;
+  text?: string;
+};
+
+export default function CustomCheckbox({
+  id,
+  checked,
+  handleCheck,
+  text,
+}: Props) {
+  return (
+    <div className={css.taskCustomCheckbox}>
+      <input
+        onChange={handleCheck}
+        className={`${css.defaultCheckbox} {visuallyHidden}`}
+        type="checkbox"
+        name="taskCheckbox"
+        id={id}
+        defaultChecked={checked}
+      />
+      <label className={css.labelCheckbox} htmlFor={id}>
+        <span className={css.customCheckbox}>
+          <svg className={css.checkboxMark} width={14} height={11}>
+            <use href="/sprite.svg#icon-mark" />
+          </svg>
+        </span>
+        <p className={css.itemText}>{text}</p>
+      </label>
+    </div>
+  );
+}
