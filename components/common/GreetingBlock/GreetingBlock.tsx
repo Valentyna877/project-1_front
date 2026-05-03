b29f7b935e794e34f576020c84f50d3ec221d75f
KarllWest <karlwest396@gmail.com>
2026-05-03 18:14:24 +0100

    Add responsive styles for Journey section

--- components/common/GreetingBlock/GreetingBlock.tsx
+++ components/common/GreetingBlock/GreetingBlock.tsx
@@ -3,11 +3,15 @@
 import css from './GreetingBlock.module.css';
 import { useAuthStore } from '@/lib/store/authStore';
 
-export default function GreetingBlock() {
+interface GreetingBlockProps {
+  prefix?: string;
+}
+
+export default function GreetingBlock({ prefix = 'Вітаю' }: GreetingBlockProps) {
   const user = useAuthStore((state) => state.user);
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
   const name = isAuthenticated && user?.name;
 
-  return <h1 className={css.greetingTitle}>Вітаю, {!user ? 'Мамо' : name}!</h1>;
+  return <h1 className={css.greetingTitle}>{prefix}, {!user ? 'Мамо' : name}!</h1>;
 }
--- components/journey/JourneyPageClient/JourneyPageClient.tsx
+++ components/journey/JourneyPageClient/JourneyPageClient.tsx
@@ -2,6 +2,7 @@
 
 import WeekSelector from "../WeekSelector/WeekSelector";
 import JourneyDetails from "../JourneyDetails/JourneyDetails";
+import GreetingBlock from "@/components/common/GreetingBlock/GreetingBlock";
 import styles from "./JourneyPageClient.module.css";
 
 interface JourneyPageClientProps {
@@ -15,6 +16,7 @@ const JourneyPageClient = ({
 }: JourneyPageClientProps) => {
   return (
     <div className={styles.wrapper}>
+      <GreetingBlock prefix="Доброго ранку" />
       <WeekSelector currentWeek={currentWeek} activeWeek={weekNumber} />
       <JourneyDetails weekNumber={weekNumber} />
     </div>
