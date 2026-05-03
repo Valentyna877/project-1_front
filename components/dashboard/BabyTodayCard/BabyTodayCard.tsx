cf565dd1ea0ee234db1d4996502ead6357e52d2c
Uffaser <gerasimenkom405@gmail.com>
2026-05-03 04:41:12 +0300

    Refactor BabyTodayCard to use a single babyInfo prop and improve error handling; update DashboardCardClient to pass babyInfo directly.

--- components/dashboard/BabyTodayCard/BabyTodayCard.tsx
+++ components/dashboard/BabyTodayCard/BabyTodayCard.tsx
@@ -1,54 +1,42 @@
 import Image from 'next/image';
 import css from './BabyTodayCard.module.css';
+import { Baby } from '@/types/weeks';
+import Loader from '@/components/common/Loader/Loader';
 
 type Props = {
-  image: string | undefined;
-  analogy: string | undefined;
-  babySize: number | undefined;
-  babyWeight: number | undefined;
-  babyActivity: string | undefined;
-  babyDevelopment: string | undefined;
-  imageAlt: string | undefined;
+  babyInfo: Baby;
 };
 
-export default function BabyTodayCard({
-  image,
-  analogy,
-  babySize,
-  babyWeight,
-  babyActivity,
-  babyDevelopment,
-  imageAlt,
-}: Props) {
+export default function BabyTodayCard({ babyInfo }: Props) {
   return (
     <div className={css.babyCardBox}>
       <h2>Малюк сьогодні</h2>
       <div className={css.babyInfoBox}>
         <Image
           className={css.babyImg}
-          src={image || '/no image'}
-          alt={analogy || imageAlt || 'no connection'}
+          src={babyInfo?.image ?? '/No connection. Sorry!'}
+          alt={
+            (babyInfo?.analogy || babyInfo?.imageAlt) ?? 'No connection. Sorry!'
+          }
           width={257}
           height={194}
         />
         <div className={css.babyTextWrapper}>
           <p className={css.babyInfo}>
             <span className={css.strong}>Розмір: </span>
-            {babySize ? `Приблизно ${babySize} см` : 'Помилка'}
+            {babyInfo?.babySize}
           </p>
           <p className={css.babyInfo}>
             <span className={css.strong}>Вага: </span>
-            {babyWeight ? `Близько ${babyWeight} грамів` : 'Помилка'}
+            {babyInfo?.babyWeight}
           </p>
           <p className={css.babyInfo}>
             <span className={css.strong}>Активність: </span>
-            {babyActivity ? `${babyActivity}` : 'Помилка'}
+            {babyInfo?.babyActivity}
           </p>
         </div>
       </div>
-      <p className={css.babyCardText}>
-        {babyDevelopment ? `${babyDevelopment}` : 'Помилка'}
-      </p>
+      <p className={css.babyCardText}>{babyInfo?.babyDevelopment}</p>
     </div>
   );
 }
--- components/dashboard/StatusBlock/StatusBlock.tsx
+++ components/dashboard/StatusBlock/StatusBlock.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@/hooks/useTheme';
 import css from './StatusBlock.module.css';
 
 interface Props {
@@ -6,13 +7,14 @@ interface Props {
 }
 
 export default function StatusBlock({ days, weeks }: Props) {
+  const { theme, themeClass } = useTheme();
   return (
     <div className={css.statusBlockBox}>
-      <div className={css.statusBox}>
+      <div className={`${css.statusBox} ${css[themeClass]}`}>
         <p className={css.statusTitle}>Тиждень</p>
         <p className={css.statusInfo}>{weeks ? `${weeks}` : 0}</p>
       </div>
-      <div className={css.statusBox}>
+      <div className={`${css.statusBox} ${css[themeClass]}`}>
         <p className={css.statusTitle}>Днів до зустрічі</p>
         <p className={css.statusInfo}>~{days ? `${days}` : 0}</p>
       </div>
