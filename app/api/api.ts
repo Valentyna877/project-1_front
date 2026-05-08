d5d1ead0b37782fc9e1b5889d67a7bf948dc0932
Valentyna <valentine177@gmail.com>
2026-05-08 04:42:37 +0300

    deploy

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api/",
+  baseURL: "https://project-1-back.onrender.com",
   withCredentials: true,
 });
